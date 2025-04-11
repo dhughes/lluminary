7d32c83e5beec9d20e61fa68f11f904278494c65
Doug Hughes <doug@doughughes.net>
2025-04-11 05:55:53 -0400

    Remove hello world example and its spec

--- spec/luminary_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'luminary'
-
-RSpec.describe Luminary do
-  describe '.hello' do
-    it 'returns a welcome message' do
-      expect(Luminary.hello).to eq('Welcome to Luminary - your LLM framework!')
-    end
-  end
-end 
