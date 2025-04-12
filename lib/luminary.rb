92fcd0972731973ab0cc41b106edf14c8dd209f9
Doug Hughes <doug@doughughes.net>
2025-04-12 07:54:25 -0400

    Remove unused schema classes - Delete InputSchema and OutputSchema classes - Delete associated test files - Remove requires from luminary.rb

--- lib/luminary.rb
+++ lib/luminary.rb
@@ -1,7 +1,5 @@
 require_relative 'luminary/version'
 require_relative 'luminary/result'
-require_relative 'luminary/input_schema'
-require_relative 'luminary/output_schema'
 require_relative 'luminary/task'
 require_relative 'luminary/providers/base'
 require_relative 'luminary/providers/openai'
